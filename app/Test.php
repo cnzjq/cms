e96f0ce657a52f386e19b4335ef184ef0a06d453
张俊强 <55357571@qq.com>
2024-10-08 11:32:07 +0800

    first commit

---
+++ app/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace app;
+
+class Test
+{
+    public function index()
+    {
+        echo 'composer包测试成功';
+    }
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+namespace zjq;
+require "vendor/autoload.php";
+use app\Test;
+
+$Test = new Test();
+$Test->index();
